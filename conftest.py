87fd51d0b3a43fa302fcb7821f9bf57d8bf2a48e
Navin <navin.islam@gmail.com>
2021-01-01 23:00:12 -0500

    dgrade html report

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import pytest
-from py._xmlgen import html
+from py.xml import html
 from page_sample import InventoryPage
 from page_sample import LoginPage
 
@@ -9,9 +9,11 @@ from page_sample import LoginPage
 def login(selenium):
     return LoginPage(selenium)
 
+
 @pytest.fixture
 def inventory(selenium):
     return InventoryPage(selenium)
+
 
 # Additional content for pytest results
 @pytest.mark.optionalhook
@@ -19,10 +21,12 @@ def pytest_html_results_table_header(cells):
     cells.insert(2, html.th('Description'))
     cells.insert(1, html.th('Time', class_='sortable time', col='time'))
 
+
 @pytest.mark.optionalhook
 def pytest_html_results_table_row(report, cells):
     cells.insert(2, html.td(report.description))
     cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+
 
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item, call):
