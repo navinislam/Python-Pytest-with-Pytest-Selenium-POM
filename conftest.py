43886ebd256c8b328afd56a9b3030841a8c24ac7
Navin <navin.islam@gmail.com>
2021-01-01 22:53:40 -0500

    html

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 import pytest
-from page_sample import LoginPage
+from py._xmlgen import html
 from page_sample import InventoryPage
+from page_sample import LoginPage
 
 
 @pytest.fixture
@@ -11,3 +13,19 @@ def login(selenium):
 def inventory(selenium):
     return InventoryPage(selenium)
 
+# Additional content for pytest results
+@pytest.mark.optionalhook
+def pytest_html_results_table_header(cells):
+    cells.insert(2, html.th('Description'))
+    cells.insert(1, html.th('Time', class_='sortable time', col='time'))
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_row(report, cells):
+    cells.insert(2, html.td(report.description))
+    cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    report.description = str(item.function.__doc__)
